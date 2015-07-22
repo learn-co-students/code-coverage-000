c655ba7444d91dad3931ce77bfb072d1ab9ab82f
Artem Eremin <artross.bodhi@gmail.com>
2015-07-22 23:47:35 +0300

    done

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -10,9 +10,41 @@ songs = [
 ]
 
 describe Song do
-
+  it "takes a name when created" do
+    song = Song.new("A song")
+    expect(song.name).to eq("A song")
+  end	
 end
 
 describe Jukebox do
+  it "takes songs when created" do
+  	expect{Jukebox.new(songs)}.to_not raise_error
+  end	
 
+  context "has methods:" do
+  	let(:juke) {Jukebox.new(songs)}
+  	
+  	it "'list' lists all its songs" do
+  	  expect(juke.command('list')).to eq(songs.each_with_index.each_with_object("") { |(song, i), str| str << "#{i+1} #{song}\n" } )
+  	end
+
+    it "'play' plays a song" do
+      song = songs[0]
+      expect(juke.command("play #{song}")).to eq("now playing #{song}")
+    end
+
+    it "'help' shows help" do
+      expect(juke.command('help')).to eq("Please select help, list, exit, or play.")
+    end
+
+  	it "'exit' turns it off" do
+  	  expect(juke.on?).to eq(true)
+  	  juke.command('exit')
+  	  expect(juke.on?).to eq(false)
+  	end
+
+  	it "responds to invalid commands" do
+      expect(juke.command("unknown")).to eq("invalid command")
+  	end
+  end
 end
